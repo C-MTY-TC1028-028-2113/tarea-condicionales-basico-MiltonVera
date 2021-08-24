c370ef51a8d1bf078862e6a09f4d001261db4318
MiltonVera <a00833471@itesm.mx>
2021-08-23 20:33:00 -0500

    Tarea

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,7 +1,21 @@
 
 def main():
     edad = int(input("Ingresa tu edad: "))
-    #Aquí empieza tu programa...
+    if(edad < 18):
+        if(edad<=0):
+            print("Respuesta incorrecta")
+        else:
+            print("No cumples requisitos")
+    else:       
+        #Aquí empieza tu programa...
+        id = input("¿Tienes identificación oficial? (s/n): ").lower()
+        if(id == "s"):
+            print("Trámite de licencia concedido")
+        elif(id == "n"):
+            print("No cumples requisitos")
+        elif(edad <= 0 or (id != "s" or id != "n")):
+            print("Respuesta incorrecta")
+
 
 if __name__ == '__main__':
     main()
--- assignments/03TipoTriangulo/src/exercise.py
+++ assignments/03TipoTriangulo/src/exercise.py
@@ -3,6 +3,16 @@ def main():
     lado2 = int(input("Ingresa la medida del lado 2: "))
     lado3 = int(input("Ingresa la medida del lado 3: "))
     #Escribe aquí tu código...
+    if((lado1+lado2)>lado3 and (lado1+lado3)>lado1 and (lado2+lado3)>lado1 and lado1>0 and lado2 > 0 and lado3>0):
+        #Empieza el programa
+        if(lado1 == lado2 == lado3):
+            print("ES UN TRIANGULO EQUILATERO")
+        elif(lado1 == lado2 or lado1 == lado3 or lado2 == lado3):
+            print("ES UN TRIANGULO ISOSCELES")
+        else:
+            print("ES UN TRIANGULO ESCALENO")
+    else:
+        print("NO ES TRIANGULO")
 
 
 if __name__=='__main__':
